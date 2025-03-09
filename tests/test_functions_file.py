63d888405819647e7b978989f0e43a03d08f5702
codemachinee <johnybritva2@gmail.com>
2025-03-10 01:57:45 +0300

    test

--- tests/test_functions_file.py
+++ tests/test_functions_file.py
@@ -8,19 +8,19 @@ import gspread
 
 
 # @pytest.mark.skip(reason="Этот тест запускается только вручную")
-@pytest.mark.asyncio
-async def test_value_plus_one():
-    try:
-        gc = gspread.service_account(filename='pidor-of-the-day-af3dd140b860.json')
-        # Пытаемся открыть таблицу "bot_statistic"
-        sh = gc.open("bot_statistic")
-
-        # Пытаемся получить первый лист (worksheet)
-        worksheet = sh.get_worksheet(0)
-        assert worksheet is not None
-        print("Подключение к таблице успешно!")
-
-    except gspread.SpreadsheetNotFound:
-        pytest.fail("Таблица 'bot_statistic' не найдена.")
-    except Exception as e:
-        pytest.fail(f"Ошибка при подключении к таблице: {str(e)}")
+# @pytest.mark.asyncio
+# async def test_value_plus_one():
+#     try:
+#         gc = gspread.service_account(filename='pidor-of-the-day-af3dd140b860.json')
+#         # Пытаемся открыть таблицу "bot_statistic"
+#         sh = gc.open("bot_statistic")
+#
+#         # Пытаемся получить первый лист (worksheet)
+#         worksheet = sh.get_worksheet(0)
+#         assert worksheet is not None
+#         print("Подключение к таблице успешно!")
+#
+#     except gspread.SpreadsheetNotFound:
+#         pytest.fail("Таблица 'bot_statistic' не найдена.")
+#     except Exception as e:
+#         pytest.fail(f"Ошибка при подключении к таблице: {str(e)}")
